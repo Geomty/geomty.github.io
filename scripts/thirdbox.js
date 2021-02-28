dae945e2be6f2f9d49bb688361b7f7bd1e890195
Geomty <69478549+Geomty@users.noreply.github.com>
2021-02-28 16:10:08 -0500

    Update thirdbox.js

--- scripts/thirdbox.js
+++ scripts/thirdbox.js
@@ -1,7 +1,7 @@
 var count = 0;
 function noMoreProjects() {
     if (count == 0) {
-        document.getElementById("geometer").innerHTML = "";
+        document.getElementById("mrfactual").innerHTML = "";
         document.getElementById("ytapi-simplifier").innerHTML = "<br><a href=\"https://github.com/Geomty/ytapi-simplifier\">A JavaScript module designed to simplify the YouTube API!</a>";
         document.getElementById("questionmarks").innerHTML = "Click me again";
         count++;
